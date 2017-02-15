4ce62abcc896a5fa2605a97da28bc9be24a06401
Panchatcharam <mrpanchu2003@gmail.com>
2017-02-15 10:31:51 +0800

    Create BuildConfig.cmake

---
+++ source/common/config/BuildConfig.cmake
@@ -0,0 +1,18 @@
+if (WIN32)
+message(STATUS "Compiling on Windows")
+elseif(UNIX)
+    if (NOT APPLE AND ${CMAKE_SYSTEM_NAME} STREQUAL "Linux") 
+    
+        message(STATUS "Compiling on Linux")
+
+        if ( CMAKE_SIZEOF_VOID_P EQUAL 8 )
+            add_definitions( -DPLATFORM64 )
+        else()      
+            add_definitions( -DPLATFORM32 )
+        endif()
+
+        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -ldl -v -pedantic -Werror -o0 -g -std=c++11" )
+        set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated -D_GLIBCXX_USE_CXX11_ABI=0 -Wno-error=sign-compare")
+    
+    endif()
+endif()
